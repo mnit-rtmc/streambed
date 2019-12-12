bd4e533fccf7b5e73fac3af9ba1813ac3940745f
Douglas Lau <doug.lau@state.mn.us>
2019-12-12 08:59:46 -0600

    Cleaned up public items.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
-pub mod error;
-pub mod stream;
+mod error;
+mod stream;
+
+pub use crate::error::Error;
+pub use crate::stream::{
+    AspectRatio, Encoding, MatrixCrop, Sink, Stream, StreamBuilder,
+    StreamControl,
+};
