2bc206e22a3a7190e925f674e6088f03e9c77c40
Douglas Lau <doug.lau@state.mn.us>
2020-01-06 09:25:15 -0600

    Added comments to error module.

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,33 @@
+// error.rs
+//
+// Copyright (C) 2019-2020  Minnesota Department of Transportation
+//
 use muon_rs::Error as MuonError;
 use std::fmt;
 use std::num::{ParseIntError, TryFromIntError};
 
+/// Streambed errors
 #[derive(Debug)]
 pub enum Error {
+    /// Missing gstreamer element
     MissingElement(&'static str),
+    /// Invalid gstreamer property
     InvalidProperty(&'static str),
+    /// Error while connecting a glib signal
     ConnectSignal(&'static str),
+    /// Error while adding an element to a pipeline
     PipelineAdd(),
-    LinkElement(),
+    /// Invalid MatrixCrop definition
     InvalidCrop(),
+    /// Error parsing integer
     ParseInt(ParseIntError),
+    /// Error converting from integer
     TryFromInt(TryFromIntError),
+    /// I/O error
     Io(std::io::Error),
+    /// Muon error
     Muon(MuonError),
+    /// Other error
     Other(&'static str),
 }
 
@@ -24,7 +38,6 @@ impl fmt::Display for Error {
             Error::InvalidProperty(e) => write!(f, "invalid property: {}", e),
             Error::ConnectSignal(e) => write!(f, "connect signal: {}", e),
             Error::PipelineAdd() => write!(f, "pipeline add"),
-            Error::LinkElement() => write!(f, "link elements"),
             Error::InvalidCrop() => write!(f, "invalid crop"),
             Error::ParseInt(e) => write!(f, "parse {:?}", e),
             Error::TryFromInt(e) => write!(f, "try_from {:?}", e),
